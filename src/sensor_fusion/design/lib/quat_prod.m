2e1fa40212e0526c094db26193e95a10e3a6ade4
Ramasamy <ram.kannan@samsung.com>
2014-04-25 14:53:39 +0530

    Moving sensor fusion octave code to sensord
      - Moving octave sensor fusion design related directory from
        sensor-framework.git to sensord.git.
      - Renamed all files and folders to lower case.
      - removed carriage return \r from all files
    
    signed-off-by: Ramasamy <ram.kannan@samsung.com>
    Change-Id: Ib5421200ca5bf0d8cb64fdbb40faae0e474455b8

---
+++ src/sensor_fusion/design/lib/quat_prod.m
@@ -0,0 +1,25 @@
+function ab = quat_prod(a, b)
+% QuatProd
+%
+% Copyright (c) 2014 Samsung Electronics Co., Ltd.
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+%
+% http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+
+% Calculates the quaternion product of quaternion a and b.
+
+    ab(:,1) = a(:,1).*b(:,1)-a(:,2).*b(:,2)-a(:,3).*b(:,3)-a(:,4).*b(:,4);
+    ab(:,2) = a(:,1).*b(:,2)+a(:,2).*b(:,1)+a(:,3).*b(:,4)-a(:,4).*b(:,3);
+    ab(:,3) = a(:,1).*b(:,3)-a(:,2).*b(:,4)+a(:,3).*b(:,1)+a(:,4).*b(:,2);
+    ab(:,4) = a(:,1).*b(:,4)+a(:,2).*b(:,3)-a(:,3).*b(:,2)+a(:,4).*b(:,1);
+end
+
