b68d286e6f2015b52d0b7f4b543dea11e58afad0
Ramasamy <ram.kannan@samsung.com>
2014-04-25 13:38:22 +0530

    Moving sensor fusion octave code to sensord
      - Moving octave sensor fusion design related directory from
        sensor-framework.git to sensord.git.
      - Renamed all files and folders to lower case.
    
    signed-off-by: Ramasamy <ram.kannan@samsung.com>
    Change-Id: I610915d6ea5d7b3de7019ae39bb3ac5f31114b89

---
+++ src/sensor_fusion/design/lib/quat2euler.m
@@ -0,0 +1,32 @@
+function euler = quat2euler(q)
+% Quat2Euler
+%
+% Copyright (c) 2014 Samsung Electronics Co., Ltd.
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+%
+% http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+
+% Converts a quaternion orientation to ZYX Euler angles where phi is a
+% rotation around X, theta around Y and psi around Z.
+
+    R(1,1,:) = 2.*q(:,1).^2-1+2.*q(:,2).^2;
+    R(2,1,:) = 2.*(q(:,2).*q(:,3)-q(:,1).*q(:,4));
+    R(3,1,:) = 2.*(q(:,2).*q(:,4)+q(:,1).*q(:,3));
+    R(3,2,:) = 2.*(q(:,3).*q(:,4)-q(:,1).*q(:,2));
+    R(3,3,:) = 2.*q(:,1).^2-1+2.*q(:,4).^2;
+
+    phi = atan2(R(3,2,:), R(3,3,:) );
+    theta = -atan(R(3,1,:) ./ sqrt(1-R(3,1,:).^2) );
+    psi = atan2(R(2,1,:), R(1,1,:) );
+
+    euler = [phi(1,:)' theta(1,:)' psi(1,:)'];
+end
