2d129410e6b23500610e0d0d12650cf008e57e85
Amit Dharmapurikar <amit.vd@samsung.com>
2014-06-03 14:14:50 +0530

    Modified accel_sensor_hal as per IIO driver interface
    The accel_sensor_hal has been modified as per IIO driver interface for Tizen 3.0.
    The plugin uses IIO common library for IIO interface related functionality.
    
    Change-Id: I732839161619e61f17aa4b1836853ad101860804
    Signed-off-by: Amit Dharmapurikar <amit.vd@samsung.com>

--- src/accel/accel_sensor_hal.h
+++ src/accel/accel_sensor_hal.h
@@ -22,17 +22,39 @@
 
 #include <sensor_hal.h>
 #include <string>
+#include <iio_common.h>
+
+#define INPUT_DEV_NAME	"lsm330dlc-accel"
+#define INPUT_TRIG_NAME	"lsm330dlc-accel-trigger"
+
+#define IIO_DIR 			"/sys/bus/iio/devices/"
+#define ACCEL_FREQ 			"sampling_frequency"
+#define ACCEL_FREQ_AVLBL	"sampling_frequency_available"
+#define ACCEL_SCALE_AVLBL	"in_accel_scale_available"
+#define ACCEL_X_SCALE		"in_accel_x_scale"
+#define ACCEL_Y_SCALE		"in_accel_y_scale"
+#define ACCEL_Z_SCALE		"in_accel_z_scale"
+
+#define NO_OF_CHANNELS		4
+#define MAX_FREQ_COUNT		16
+#define MAX_SCALING_COUNT	16
+
+#define CHANNEL_NAME_X		"in_accel_x"
+#define CHANNEL_NAME_Y		"in_accel_y"
+#define CHANNEL_NAME_Z		"in_accel_z"
+#define CHANNEL_NAME_TIME	"in_timestamp"
+#define ENABLE_SUFFIX		"_en"
+#define NAME_NODE			"/name"
+#define BUFFER_EN			"buffer/enable"
+#define BUFFER_LEN			"buffer/length"
+#define SCAN_EL_DIR			"scan_elements/"
+
+#define ACCEL_RINGBUF_LEN	32
 
 using std::string;
 
 class accel_sensor_hal : public sensor_hal
 {
-	enum accel_cmd_cal_t {
-		CAL_CHECK = 0,
-		CAL_SET,
-		CAL_MKDIR,
-	};
-
 public:
 	accel_sensor_hal();
 	virtual ~accel_sensor_hal();
@@ -40,7 +62,7 @@ public:
 	sensor_type_t get_type(void);
 	bool enable(void);
 	bool disable(void);
-	bool set_interval(unsigned long val);
+	bool set_interval(unsigned long ms_interval);
 	bool is_data_ready(bool wait);
 	virtual int get_sensor_data(sensor_data_t &data);
 	bool get_properties(sensor_properties_t &properties);
@@ -51,7 +73,6 @@ private:
 	int m_x;
 	int m_y;
 	int m_z;
-	int m_node_handle;
 	unsigned long m_polling_interval;
 	unsigned long long m_fired_time;
 	bool m_sensorhub_supported;
@@ -61,18 +82,36 @@ private:
 	string m_vendor;
 	string m_chip_name;
 
+	string m_polling_resource;
+
+	string m_accel_dir;
+	string m_accel_trig_dir;
+	string m_buffer_access;
+	string m_freq_resource;
+
 	int m_resolution;
 	float m_raw_data_unit;
 
-	string m_resource;
-	string m_enable_resource;
-	string m_polling_resource;
+	int m_scale_factor_count;
+	int m_sample_freq_count;
+	int m_sample_freq[MAX_FREQ_COUNT];
+	double m_scale_factor[MAX_SCALING_COUNT];
+
+	int m_fp_buffer;
+	char *m_data;
+	int m_scan_size;
+	struct channel_parameters *m_channels;
 
 	cmutex m_value_mutex;
 
-	bool enable_resource(string &resource_node, bool enable);
 	bool update_value(bool wait);
-	bool calibration(int cmd);
 	bool is_sensorhub_supported(void);
+
+	bool enable_resource(bool enable);
+	bool add_accel_channels_to_array(void);
+	bool setup_channels(void);
+	bool setup_buffer(int enable);
+	bool setup_trigger(char* trig_name, bool verify);
+	void decode_data(void);
 };
 #endif /*_ACCEL_SENSOR_HAL_H_*/
