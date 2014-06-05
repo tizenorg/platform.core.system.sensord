73d75e322a7ac1cad03ca5829bd08bebee6f0576
Ramasamy <ram.kannan@samsung.com>
2014-06-05 17:24:52 +0530

    Adding matrix class and template
    - Adding standalone directory for sensor fusion standalone C/C++
      library
    - Adding matrix class definition and template matrix cpp file.
    
    signed-off-by: Ramasamy <ram.kannan@samsung.com>
    Change-Id: I55e4244338a33ad89272600bdcfad2605dff4af1

---
+++ src/sensor_fusion/standalone/util/matrix.h
@@ -0,0 +1,48 @@
+/*
+ * sensord
+ *
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef _MATRIX_H
+#define _MATRIX_H
+
+class matrix {
+public:
+	int rows;
+	int cols;
+	float **mat;
+
+	matrix(void);
+	matrix(const int rows, const int cols);
+	matrix(const int rows, const int cols, float **mat_data);
+	matrix(const matrix& m);
+	~matrix();
+
+	matrix operator =(const matrix& m);
+
+	friend matrix operator +(const matrix m1, const matrix m2);
+	friend matrix operator +(const matrix m, const float val);
+	friend matrix operator -(const matrix m1, const matrix m2);
+	friend matrix operator -(const matrix m, const float val);
+	friend matrix operator *(const matrix m1, const matrix m2);
+	friend matrix operator *(const matrix m, const float val);
+	friend matrix operator /(const matrix m1, const matrix m2);
+	friend bool operator ==(const matrix m1, const matrix m2);
+	friend bool operator !=(const matrix m1, const matrix m2);
+};
+
+#endif  //_MATRIX_H
