2c884254fd436556f969b622a4409b18e88d38f5
Ramasamy <ram.kannan@samsung.com>
2014-06-05 17:24:52 +0530

    Adding matrix class header
    - Adding matrix class definition for sensor fusion util library
    
    signed-off-by: Ramasamy <ram.kannan@samsung.com>
    Change-Id: I55e4244338a33ad89272600bdcfad2605dff4af1

---
+++ src/sensor_fusion/standalone/util/matrix.h
@@ -0,0 +1,44 @@
+/*
+ * sensord
+ *
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef _MATRIX_H
+#define _MATRIX_H
+
+#include<stdlib.h>  
+
+class matrix {
+public:
+	int rows;
+	int cols;
+	float **mat;
+	
+private:
+	void matrix(int rows, int cols);
+	matrix(const matrix& m);
+	matrix operator = (const matrix& m);
+	matrix operator +(const matrix m);
+	matrix operator +(const float val);
+	matrix operator -(const matrix m);
+	matrix operator -(const float val);
+	matrix operator *(const matrix m);
+	matrix operator *(const float val); 
+	matrix operator /(const matrix m);
+};
+
+#endif  //_MATRIX_H
