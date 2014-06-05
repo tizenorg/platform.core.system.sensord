ac511ace8dd441e3b85632e2dfbf377c0c3c94c4
Ramasamy <ram.kannan@samsung.com>
2014-06-05 17:24:52 +0530

    Adding template matrix class
    - Adding standalone directory for sensor fusion standalone C/C++
      library
    - Adding matrix template class definition.
    
    signed-off-by: Ramasamy <ram.kannan@samsung.com>
    Change-Id: I55e4244338a33ad89272600bdcfad2605dff4af1

---
+++ src/sensor_fusion/standalone/util/matrix.cpp
@@ -0,0 +1,117 @@
+/*
+ * sensord
+ *
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <matrix.h>
+
+template <typename TYPE>
+matrix<TYPE>::matrix(void)
+{
+
+}
+
+template <typename TYPE>
+matrix<TYPE>::matrix(const int rows, const int cols)
+{
+
+}
+
+template <typename TYPE>
+matrix<TYPE>::matrix(const int rows, const int cols, TYPE **mat_data)
+{
+
+}
+
+template <typename TYPE>
+matrix<TYPE>::matrix(const matrix<TYPE>& m)
+{
+
+}
+
+template <typename TYPE>
+matrix<TYPE>::~matrix()
+{
+
+}
+
+template <typename TYPE>
+matrix<TYPE> matrix<TYPE>::operator =(const matrix<TYPE>& m)
+{
+
+}
+
+template <typename TYPE>
+matrix<TYPE> operator +(const matrix<TYPE> m1, const matrix<TYPE> m2)
+{
+
+}
+
+template <typename TYPE>
+matrix<TYPE> operator +(const matrix<TYPE> m, const TYPE val)
+{
+
+}
+
+template <typename TYPE>
+matrix<TYPE> operator -(const matrix<TYPE> m1, const matrix<TYPE> m2)
+{
+
+}
+
+template <typename TYPE>
+matrix<TYPE> operator -(const matrix<TYPE> m, const TYPE val)
+{
+
+}
+
+template <typename TYPE>
+matrix<TYPE> operator *(const matrix<TYPE> m1, const matrix<TYPE> m2)
+{
+
+}
+
+template <typename TYPE>
+matrix<TYPE> operator *(const matrix<TYPE> m, const TYPE val)
+{
+
+}
+
+template <typename TYPE>
+matrix<TYPE> operator /(const matrix<TYPE> m1, const matrix<TYPE> m2)
+{
+
+}
+
+template <typename TYPE>
+bool operator ==(const matrix<TYPE> m1, const matrix<TYPE> m2)
+{
+
+}
+
+template <typename TYPE>
+bool operator !=(const matrix<TYPE> m1, const matrix<TYPE> m2)
+{
+
+}
+
+
+template <typename TYPE>
+matrix<TYPE> matrix<TYPE>::transpose(const matrix<TYPE> m)
+{
+
+}
