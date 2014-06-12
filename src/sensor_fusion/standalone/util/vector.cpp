fd8d8ffbc980fc0d997a01578c0b5f9b66c1c7ca
Ramasamy <ram.kannan@samsung.com>
2014-06-12 11:45:32 +0530

    Adding vector template class
    - Adding vector template class definition
    
    signed-off-by: Ramasamy <ram.kannan@samsung.com>
    Change-Id: Ia750b33d5a5b852adfd31fe4ed965c5d9ca070ed

---
+++ src/sensor_fusion/standalone/util/vector.cpp
@@ -0,0 +1,122 @@
+/*
+ * sensord
+ *
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <vector.h>
+
+template <typename TYPE>
+vector<TYPE>::vector(void)
+{
+
+}
+
+template <typename TYPE>
+vector<TYPE>::vector(const int size)
+{
+
+}
+
+template <typename TYPE>
+vector<TYPE>::vector(const int size, TYPE *vec_data)
+{
+
+}
+
+template <typename TYPE>
+vector<TYPE>::vector(const vector<TYPE>& v)
+{
+
+}
+
+template <typename TYPE>
+vector<TYPE>::~vector()
+{
+
+}
+
+template <typename TYPE>
+vector<TYPE> vector<TYPE>::operator =(const vector<TYPE>& v)
+{
+
+}
+
+template <typename TYPE>
+vector<TYPE> operator +(const vector<TYPE> v1, const vector<TYPE> v2)
+{
+
+}
+
+template <typename TYPE>
+vector<TYPE> operator +(const vector<TYPE> v, const TYPE val)
+{
+
+}
+
+template <typename TYPE>
+vector<TYPE> operator -(const vector<TYPE> v1, const vector<TYPE> v2)
+{
+
+}
+
+template <typename TYPE>
+vector<TYPE> operator -(const vector<TYPE> v, const TYPE val)
+{
+
+}
+
+template <typename TYPE>
+matrix<TYPE> operator *(const matrix<TYPE> m, const vector<TYPE> v)
+{
+
+}
+
+template <typename TYPE>
+TYPE operator *(const vector<TYPE> v, const matrix<TYPE> m)
+{
+
+}
+
+template <typename TYPE>
+vector<TYPE> operator *(const vector<TYPE> v, const TYPE val)
+{
+
+}
+
+template <typename TYPE>
+vector<TYPE> operator /(const vector<TYPE> v1, const vector<TYPE> v2)
+{
+
+}
+
+template <typename TYPE>
+bool operator ==(const vector<TYPE> v1, const vector<TYPE> v2)
+{
+
+}
+
+template <typename TYPE>
+bool operator !=(const vector<TYPE> v1, const vector<TYPE> v2)
+{
+
+}
+
+template <typename TYPE>
+matrix<TYPE> vector<TYPE>::transpose(const vector<TYPE> v)
+{
+
+}
