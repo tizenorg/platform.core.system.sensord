c42cdd17de209c0fcbf21da92a61d6d7d9bc600a
Ramasamy <ram.kannan@samsung.com>
2014-07-24 13:57:04 +0530

    Adding orientation_sensor virtual sensor class
     - Adding class definition and implementation for orientation virtual
       sensor
     - Adding test code for orientation_sensor class
     - interface to sensor framework for virtual class will be added
       during porting
    
    signed-off-by: Ramasamy <ram.kannan@samsung.com>
    Change-Id: I17becdbe159345c446e6944596900db4557d51ba

---
+++ src/sensor_fusion/standalone/orientation_sensor.h
@@ -0,0 +1,38 @@
+/*
+ * sensord
+ *
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef _ORIENTATION_SENSOR_H
+#define _ORIENTATION_SENSOR_H
+
+#include "./util/orientation_filter.h"
+
+class orientation_sensor
+{
+public:
+	orientation_filter<float> orien_filter;
+
+	euler_angles<float> get_orientation(const sensor_data<float> accel,
+			const sensor_data<float> gyro, const sensor_data<float> magnetic);
+	rotation_matrix<float> get_rotation_matrix(const sensor_data<float> accel,
+			const sensor_data<float> gyro, const sensor_data<float> magnetic);
+};
+
+#include "orientation_sensor.cpp"
+
+#endif /* _ORIENTATION_SENSOR_H */
