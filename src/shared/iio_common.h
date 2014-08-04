dcdd19b5269657bb28de15deace826892c24fc42
Amit Dharmapurikar <amit.vd@samsung.com>
2014-08-04 12:20:04 +0530

    Adding IIO event related data structures for non-trigger based IIO drivers
      IIO event data structure and other event decoding data types have been defined for sensor drivers without IIO trigger.
    
    Change-Id: I38e6d78ee907f47f3544fdacfcffc41d7e5d1ae6
    Signed-off-by: Amit Dharmapurikar <amit.vd@samsung.com>

--- src/shared/iio_common.h
+++ src/shared/iio_common.h
@@ -20,6 +20,18 @@
 #ifndef IIO_COMMON_H_
 #define IIO_COMMON_H_
 
+#include <linux/ioctl.h>
+
+#define NO_OF_ULL_BYTES		8
+#define NO_OF_SHORT_VAL		4
+#define CH0_INDEX			0
+#define CH1_INDEX			1
+
+#define GET_DIFF_BIT(val)	(((unsigned short)(val) >> 7) & 0x01)
+#define GET_DIR_VAL(val)	((val) & 0x0F)
+
+#define IOCTL_IIO_EVENT_FD _IOR('i', 0x90, int)
+
 struct channel_parameters {
 	char *prefix_str;
 	float scale;
@@ -34,6 +46,27 @@ struct channel_parameters {
 	unsigned int is_en;
 	unsigned int buf_index;
 };
+
+typedef struct iio_event_struct
+{
+	unsigned long long int	event_id;
+	long long int time_stamp;
+} iio_event_t;
+
+typedef enum event_id_field
+{
+	CH_TYPE = 4,
+	MODIFIER,
+	DIRECTION,
+	EVENT_TYPE,
+} event_id_field_t;
+
+typedef union ull_bytes
+{
+	unsigned long long num;
+	short int channels[NO_OF_SHORT_VAL];
+	unsigned char bytes[NO_OF_ULL_BYTES];
+} ull_bytes_t;
 
 void sort_channels_by_index(struct channel_parameters *channels, int count);
 int decode_channel_data_type(const char *device_dir, const char *ch_name, struct channel_parameters *ch_info);
