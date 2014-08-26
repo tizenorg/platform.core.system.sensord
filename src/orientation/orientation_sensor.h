3c714786bd32f5db6658be61ccf612e18d7b6f14
Ramasamy <ram.kannan@samsung.com>
2014-08-26 16:26:17 +0530

    Implementing sensor framework orientation_sensor using sensor fusion
     - Adding orientation_sensor code using software sensor fusion to
       sensor framework
     - Adding CMakelist file for orientation_sensor
     - Currently code is not added as part of the default build for
       sensord as this has to be tested and debugged once magnetometer
       plugin code is merged
    
    signed-off-by: Ramasamy <ram.kannan@samsung.com>
    Change-Id: I89aaf1346674e62130329a5965ba3158fffbd74a

---
+++ src/orientation/orientation_sensor.h
@@ -0,0 +1,60 @@
+/*
+ * sensord
+ *
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef _ORIENTATION_SENSOR_H_
+#define _ORIENTATION_SENSOR_H_
+
+#include <sensor.h>
+#include <virtual_sensor.h>
+#include <orientation_filter.h>
+
+class orientation_sensor : public virtual_sensor {
+public:
+	orientation_sensor();
+	~orientation_sensor();
+
+	bool init(void);
+	bool on_start(void);
+	bool on_stop(void);
+
+	void synthesize(const sensor_event_t &event, vector<sensor_event_t> &outs);
+
+	bool add_interval(int client_id, unsigned int interval);
+	bool delete_interval(int client_id);
+	bool get_properties(sensor_properties_t &properties);
+	sensor_type_t get_type(void);
+
+	int get_sensor_data(const unsigned int data_id, sensor_data_t &data);
+
+private:
+	sensor_base *m_accel_sensor;
+	sensor_base *m_gyro_sensor;
+	sensor_base *m_magnetic_sensor;
+
+	orientation_filter<float> m_orientation;
+
+	unsigned int m_enable_orientation;
+
+	float m_roll;
+	float m_pitch;
+	float m_yaw;
+	unsigned long long m_timestamp;
+};
+
+#endif /* _ORIENTATION_SENSOR_H_ */
