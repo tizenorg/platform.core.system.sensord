844e92bc491bbcd1c4ba65b68ce9d03794ac05da
Ramasamy <ram.kannan@samsung.com>
2014-08-26 15:29:56 +0530

    Supporting Virtual Sensor data based only on event types
     - Sensor data representation based only on sensor event types
     - removing redundant representation for sensor data using data id
    
    signed-off-by: Ramasamy <ram.kannan@samsung.com>
    Change-Id: Id2d530cede56ec4133b9db152f94a84a6c36d8e5

--- src/libsensord/sensor_gravity.h
+++ src/libsensord/sensor_gravity.h
@@ -36,10 +36,6 @@ extern "C"
  * @{
  */
 
-enum gravity_data_id {
-	GRAVITY_BASE_DATA_SET 				= (GRAVITY_SENSOR << 16) | 0x0001,
-};
-
 enum gravity_event_type {
 	GRAVITY_EVENT_RAW_DATA_REPORT_ON_TIME					= (GRAVITY_SENSOR << 16) | 0x0002,
 };
