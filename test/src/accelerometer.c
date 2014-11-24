0454a67d2880c8d7527b8d54eaaf673a46e0c6da
Vibhor Gaur <vibhor.gaur@samsung.com>
2014-11-24 17:35:55 +0530

    Updating accelerometer test file for compatibility with changes in public code
    Change-Id: I601ca6fb00d046fcf8fb38cfb17cd6bd7f13a2e4

--- test/src/accelerometer.c
+++ test/src/accelerometer.c
@@ -20,7 +20,8 @@
 #include <glib.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <sensor.h>
+#include <sensor_internal.h>
+#include <sensor_internal_deprecated.h>
 #include <stdbool.h>
 
 static GMainLoop *mainloop;
@@ -35,14 +36,9 @@ void printformat()
 {
 	printf("Usage : ./accelerometer <event> <interval>(optional)\n\n");
 	printf("event:\n");
-	printf("ROTATION_CHECK\n");
+
 	printf("RAW_DATA_REPORT_ON_TIME\n");
-	printf("CALIBRATION_NEEDED\n");
-	printf("SET_HORIZON\n");
-	printf("SET_WAKEUP\n");
-	printf("ORIENTATION_DATA_REPORT_ON_TIME\n");
-	printf("LINEAR_ACCELERATION_DATA_REPORT_ON_TIME\n");
-	printf("GRAVITY_DATA_REPORT_ON_TIME\n\n");
+
 	printf("interval:\n");
 	printf("The time interval should be entered based on the sampling frequency supported by accelerometer driver on the device in ms.If no value for sensor is entered default value by the driver will be used.\n");
 }
@@ -64,30 +60,10 @@ int main(int argc,char **argv)
 		return 0;
 	}
 
-	if (strcmp(argv[1], "ROTATION_CHECK") == 0) {
-		event = ACCELEROMETER_EVENT_ROTATION_CHECK;
-	}
-	else if (strcmp(argv[1], "RAW_DATA_REPORT_ON_TIME") == 0) {
+	if (strcmp(argv[1], "RAW_DATA_REPORT_ON_TIME") == 0) {
 		event = ACCELEROMETER_EVENT_RAW_DATA_REPORT_ON_TIME;
 	}
-	else if (strcmp(argv[1], "CALIBRATION_NEEDED") == 0) {
-		event = ACCELEROMETER_EVENT_CALIBRATION_NEEDED;
-	}
-	else if (strcmp(argv[1], "SET_HORIZON") == 0) {
-		event = ACCELEROMETER_EVENT_SET_HORIZON;
-	}
-	else if (strcmp(argv[1], "SET_WAKEUP") == 0) {
-		event = ACCELEROMETER_EVENT_SET_WAKEUP;
-	}
-	else if (strcmp(argv[1], "ORIENTATION_DATA_REPORT_ON_TIME") == 0) {
-		event = ACCELEROMETER_EVENT_ORIENTATION_DATA_REPORT_ON_TIME;
-	}
-	else if (strcmp(argv[1], "LINEAR_ACCELERATION_DATA_REPORT_ON_TIME") == 0) {
-		event = ACCELEROMETER_EVENT_LINEAR_ACCELERATION_DATA_REPORT_ON_TIME;
-	}
-	else if (strcmp(argv[1], "GRAVITY_DATA_REPORT_ON_TIME") == 0) {
-		event = ACCELEROMETER_EVENT_GRAVITY_DATA_REPORT_ON_TIME;
-	}
+
 	else {
 		printformat();
 		free(event_condition);
@@ -95,7 +71,7 @@ int main(int argc,char **argv)
 	}
 	if (argc == 3)
 		event_condition->cond_value1 = atof(argv[2]);
-	
+
 	handle = sf_connect(type);
 	result = sf_register_event(handle, event, event_condition, callback, NULL);
 
