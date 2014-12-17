13e47c777e38df4988b7cdcbae3af6fd1ce592f8
Vibhor Gaur <vibhor.gaur@samsung.com>
2014-12-17 17:56:07 +0530

    Adding polling based support for testing gyroscope sensor
    Change-Id: Ibff596783a3d5c69f8fedc284df2f46cea144315

--- test/src/gyro.c
+++ test/src/gyro.c
@@ -16,27 +16,31 @@
  * limitations under the License.
  *
  */
-
 #include <time.h>
 #include <glib.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sensor_internal.h>
 #include <stdbool.h>
+#include <sensor_common.h>
+#include <unistd.h>
 
 static GMainLoop *mainloop;
 
 void callback(unsigned int event_type, sensor_event_data_t *event, void *user_data)
 {
 	sensor_data_t *data = (sensor_data_t *)event->event_data;
-	printf("Gyroscope [%lld] [%6.6f] [%6.6f] [%6.6f] \n\n", data->timestamp, data->values[0], data->values[1], data->values[2]);
+	printf("Gyroscope [%lld] [%6.6f] [%6.6f] [%6.6f]\n\n", data->timestamp, data->values[0], data->values[1], data->values[2]);
 }
 
 void printformat()
 {
 	printf("Usage : ./gyro <event> <interval>(optional)\n\n");
 	printf("event:\n");
-	printf("RAW_DATA_REPORT_ON_TIME\n");
+
+	printf("POLLING_BASED\n");
+	printf("EVENT_BASED\n");
+
 	printf("interval:\n");
 	printf("The time interval should be entered based on the sampling frequency supported by gyroscope driver on the device in ms.If no value for sensor is entered default value by the driver will be used.\n");
 }
@@ -45,62 +49,86 @@ int main(int argc,char **argv)
 {
 	int result, handle, start_handle, stop_handle;
 	unsigned int event;
-
 	mainloop = g_main_loop_new(NULL, FALSE);
-	sensor_type_t type = GYROSCOPE_SENSOR;
+	event = GYROSCOPE_EVENT_RAW_DATA_REPORT_ON_TIME;
 	event_condition_t *event_condition = (event_condition_t*) malloc(sizeof(event_condition_t));
 	event_condition->cond_op = CONDITION_EQUAL;
-	event_condition->cond_value1 = 10.52;
+
+	sensor_type_t type = GYROSCOPE_SENSOR;
 
 	if (argc != 2 && argc != 3) {
 		printformat();
 		free(event_condition);
 		return 0;
 	}
-	else {
-		 if (strcmp(argv[1], "RAW_DATA_REPORT_ON_TIME") == 0)
-			event = GYROSCOPE_EVENT_RAW_DATA_REPORT_ON_TIME;
 
-		 else {
-			printformat();
-			free(event_condition);
-			return 0;
+	else if (strcmp(argv[1], "POLLING_BASED") == 0) {
+		handle = sf_connect(type);
+		result = sf_start(handle, 1);
+
+		if (result < 0) {
+			printf("Can't start Gyroscope Sensor\n");
+			printf("Error\n\n\n\n");
+			return -1;
 		}
 
-		if(argc == 3)
+		sensor_data_t data;
+
+		while(1) {
+			result = sf_get_data(handle, GYRO_BASE_DATA_SET , &data);
+			printf("Gyroscope [%lld] [%6.6f] [%6.6f] [%6.6f]\n\n", data.timestamp, data.values[0], data.values[1], data.values[2]);			
+			usleep(100000);
+		}
+
+		result = sf_disconnect(handle);
+
+		if (result < 0) {
+			printf("Can't disconnect Gyroscope Sensor\n");
+			printf("Error\n\n\n\n");
+			return -1;
+		}
+	}
+
+	else if (strcmp(argv[1], "EVENT_BASED") == 0) {
+		event_condition->cond_value1 = 100;
+
+		if (argc == 3)
 			event_condition->cond_value1 = atof(argv[2]);
-	}
 
-	handle = sf_connect(type);
-	result = sf_register_event(handle, event, event_condition, callback, NULL);
+		handle = sf_connect(type);
+		result = sf_register_event(handle, event, event_condition, callback, NULL);
 
-	if (result < 0)
-		printf("Can't register gyroscope\n");
+		if (result < 0)
+			printf("Can't register gyroscope\n");
 
-	start_handle = sf_start(handle, 0);
+		start_handle = sf_start(handle,0);
 
-	if (start_handle < 0) {
-		printf("Error\n\n\n\n");
+		if (start_handle < 0) {
+			printf("Error\n\n\n\n");
+			sf_unregister_event(handle, event);
+			sf_disconnect(handle);
+			return -1;
+		}
+
+		g_main_loop_run(mainloop);
+		g_main_loop_unref(mainloop);
+
 		sf_unregister_event(handle, event);
+
+		stop_handle = sf_stop(handle);
+
+		if (stop_handle < 0) {
+			printf("Error\n\n");
+			return -1;
+		}
+
 		sf_disconnect(handle);
-		return -1;
+		free(event_condition);
 	}
 
-	g_main_loop_run(mainloop);
-	g_main_loop_unref(mainloop);
-
-	sf_unregister_event(handle, event);
-	stop_handle = sf_stop(handle);
-
-	if (stop_handle < 0) {
-		printf("Error\n\n");
-		return -1;
+	else {
+		printformat();
 	}
-
-	sf_disconnect(handle);
-
-	free(event_condition);
-
 	return 0;
 }
 
