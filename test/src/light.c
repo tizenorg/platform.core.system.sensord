4d449aaba1fcd1965158dca6450cc4814b2a1b43
Vibhor Gaur <vibhor.gaur@samsung.com>
2015-01-07 16:37:37 +0530

    Fixing memory leak issues in light test file
    Change-Id: I92b7e5303dfe2e06bab1125938f8690939983022

--- test/src/light.c
+++ test/src/light.c
@@ -75,6 +75,7 @@ int main(int argc,char **argv)
 		if (result < 0) {
 			printf("Can't start light SENSOR\n");
 			printf("Error\n\n\n\n");
+			free(event_condition);
 			return -1;
 		}
 
@@ -90,6 +91,7 @@ int main(int argc,char **argv)
 		if (result < 0) {
 			printf("Can't disconnect light sensor\n");
 			printf("Error\n\n\n\n");
+			free(event_condition);
 			return -1;
 		}
 	}
@@ -113,6 +115,7 @@ int main(int argc,char **argv)
 			printf("Error\n\n\n\n");
 			sf_unregister_event(handle, event);
 			sf_disconnect(handle);
+			free(event_condition);
 			return -1;
 		}
 
@@ -125,6 +128,7 @@ int main(int argc,char **argv)
 
 		if (stop_handle < 0) {
 			printf("Error\n\n");
+			free(event_condition);
 			return -1;
 		}
 
