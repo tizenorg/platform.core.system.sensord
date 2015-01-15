bd6d4c26bb79f3c7744fe814a6532082f22ff7aa
Vibhor Gaur <vibhor.gaur@samsung.com>
2015-01-15 20:01:18 +0530

    Mofifying vector_main file with changes in vector.cpp and vector.h
    -Changing test cases in vector_main file with changes in template structure for vector.cpp and vector.h files
    
    Change-Id: Ibcb6f7b4b9a45d9c407c32aa95f31128f3353d88

--- src/sensor_fusion/test/test_projects/vector_test/vector_main.cpp
+++ src/sensor_fusion/test/test_projects/vector_test/vector_main.cpp
@@ -31,24 +31,32 @@ int main()
 	float arr43[6] = {2.3454,-0.0384,-8.90,3.455,6.785,21.345};
 	float arr5[5] = {0.2,-0.4,0.6,-0.8,1.0};
 
-	vect<float> v1(5, arr0);
-	vect<float> v2(5, arr8);
-	vect<float> v10(4, arr3);
-	vect<float> v12(4, arr4);
-	vect<float> v15(6, arr1);
-	vect<float> v20(6, arr43);
-	vect<float> v21(3, arr2);
-	vect<float> v22(3, arr15);
-	vect<float> v3(4);
-	vect<float> v6(3);
-	vect<float> v13(5);
-	vect<float> v95(6);
-	vect<float> v35(5, arr5);
+	vect<float,5> v1(arr0);
+	vect<float,5> v2(arr8);
+	vect<float,4> v10(arr3);
+	vect<float,4> v12(arr4);
+	vect<float,6> v15(arr1);
+	vect<float,6> v20(arr43);
+	vect<float,3> v21(arr2);
+	vect<float,3> v22(arr15);
+	vect<float,5> v31(arr0);
+	vect<float,3> v3;
+	vect<float,2> vn;
+	vect<float,4> v61(arr4);
+	vect<float,3> v6;
+	vect<float,4> vm;
+	vect<float,5> v13;
+	vect<float,6> v95;
+	vect<float,5> v35(arr5);
+	vect<float,5> vl;
+	vect<float,4> vp;
+	vect<float,4> vr(arr4);
+	vect<float,3> vf;
 
 	float arr57[3][3] = {{2.24, 0.5, 0.023}, {3.675, 5.32, 0.556}, {1.023, 45.75, 621.6}};
-	matrix<float> m12(3, 3, (float *) arr57);
+	matrix<float,3 ,3> m12(arr57);
 	float arr67[3][1] = {{2.0}, {3.0}, {4.0}};
-	matrix<float> m32(3, 1, (float *) arr67);
+	matrix<float,3,1> m32(arr67);
 
 	cout<< "Constructor Test\n";
 	cout<< "\n" << v3;
@@ -56,39 +64,41 @@ int main()
 	cout<< "\n\nAddition\n";
 	v3 = v21 + v22;
 	v95 = v15 + v20;
+	cout<< "\n\nNumbers added\n";
 	cout<< "\n" << v21 << "\n" << v22;
 	cout<< "\nSum:\n" << v3 << endl;
+	cout<< "\n\nNumbers added\n";
 	cout<< "\n" << v15 << "\n" << v20;
 	cout<< "\nSum:\n" << v95 << endl;
 
 	float num = 5.5650;
 	float num1 = -2.32;
 	cout<< "\n\n\nScalar addition\n";
-	v3 = v2 + num;
-	v6 = v10 + num1;
+	vl = v2 + num;
+	vm = v10 + num1;
 	cout<< "\nNumber added:" << num;
 	cout<< "\n\n" << v2;
-	cout<< "\nResult:\n\n" << v3;
+	cout<< "\nResult:\n\n" << vl;
 	cout<< "\nNumber added:"<< num1;
 	cout<< "\n\n" << v10;
-	cout<< "\nResult:\n\n"<< v6;
+	cout<< "\nResult:\n\n"<< vm;
 
 	cout<< "\n\n\nSubtraction\n";
-	v6 = v10 - v12;
+	vp = v10 - v12;
 	cout<< "\n" << v10 << "\n" << v12;
-	cout<< "\nDifference:\n" << v6 << endl;
+	cout<< "\nDifference:\n" << vp << endl;
 
 	float x = 4.0;
 	float x1 = -2.5;
 	cout<< "\n\n\nScalar subtraction\n";
 	v13 = v1 - x;
-	v6 = v10 - x1;
+	vp = v10 - x1;
 	cout<< "\nNumber Subtracted:" << x;
 	cout<< "\n\n" << v1;
 	cout<< "\nResult:\n\n" << v13;
 	cout<< "\nNumber Subtracted:" << x1;
 	cout<< "\n\n" << v10;
-	cout<< "\nResult:\n\n" << v6;
+	cout<< "\nResult:\n\n" << vp;
 
 	float xx = 7.2;
 	cout<<"\n\n\nMultiplication\n";
@@ -97,12 +107,12 @@ int main()
 	cout<< "\nProduct:\n" << v13 << endl;
 
 	cout<< "\n\n\nMultiplication matrix x vector:\n";
-	matrix<float> m102 = m32 * v22;
+	matrix<float,3,3> m102 = m32 * v22;
 	cout<< "\n" << m32 <<"\n"<< v22;
 	cout<< "\nProduct:\n"<< m102 << endl;
 
 	cout<< "\n\n\nVector x Multiplication matrix:\n";
-	vect<float> v102 = (v22 * m12);
+	vect<float,3> v102 = (v22 * m12);
 	cout<< "\n" << v22 << "\n" << m12;
 	cout<< "\nProduct:\n" << v102 << endl;
 	float val = mul(v22, m32);
@@ -113,7 +123,7 @@ int main()
 	float z1 = -5.567;
 	cout<< "\n\n\nScalar multiplication\n";
 	v13 = v1 * z;
-	v6 = v12 * z1;
+	v61 = v12 * z1;
 	cout<< "\nNumber Multiplied:" << z;
 	cout<< "\n\n" << v1;
 	cout<< "\nResult:\n\n" << v13;
@@ -123,18 +133,18 @@ int main()
 
 	float num2 = 5.5;
 	cout<< "\n\n\nDivision\n";
-	v3 = v1 / num2;
+	v31 = v1 / num2;
 	cout<< "\n" << v1 << "\n" << num2;
 	cout<< "\nResult:\n" << v3 << endl;
 
 	cout<< "\n\n\nTranspose:";
 	cout << "\n\n" << v20;
 	cout << "\nResult:\n\n";
-	matrix<float> m101 = (transpose(v20));
+	matrix<float,6,1> m101 = transpose(v20);
 	cout << m101;
 	cout << "\n\n" << m101;
 	cout << "\nResult:\n\n";
-	v20 = (transpose(m101));
+	v20 = transpose(m101);
 	cout << v20;
 
 	cout << "\n\nv1:\n\n" << v1;
@@ -148,9 +158,9 @@ int main()
 	cout << (v10 == v10);
 
 	cout << "\n\nv12:\n\n" << v12;
-	cout << "\n\nv15:\n\n" << v15;
+	cout << "\n\nv15:\n\n" << vr;
 	cout << "\n\n\nv12 != v15 :";
-	cout << (v12 != v15);
+	cout << (v12 != vr);
 
 	cout << "\n\nv15:\n\n" << v15;
 	cout << "\n\nv15:\n\n" << v15;
@@ -158,12 +168,11 @@ int main()
 	cout << (v15 != v15);
 
 	cout<< "\n\nAssignment\n";
-	v3 = v1;
+	v3 = vf;
 	cout<< "Input \n" << v1;
 	cout<< "\nOutput:\n" << v3 << endl;
 
-
-	vect<float> v111 = cross(v21, v22);
+	vect<float,3> v111 = cross(v21, v22);
 	cout<< "\n\n\nCross Product:";
 	cout << "\n\n" << v21 << "\n\n" << v22;
 	cout << "\nResult:\n\n" << v111;
@@ -180,6 +189,6 @@ int main()
 
 	cout <<  "\n\n\nIs Initialized:";
 	cout << "\nInput:\n\n" << v35;
-	cout << "\nResult:\n\n" << is_initialized(v35);
+	cout << "\nResult:\n\n" << is_initialized(v35) << endl;
 }
 
