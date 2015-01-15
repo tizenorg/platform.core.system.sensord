2cd150feb2e318f95c54d5efb8b8a80e576bd92b
Vibhor Gaur <vibhor.gaur@samsung.com>
2015-01-15 12:42:09 +0530

    Changed vector template class structure
    -Shifting allocation of the vector size from run time to compile time by changing template structure.
    -Improvement in performance is about 8 times in executio9n time as observed after running both initial and final tests for vector class which is vector_main.
    
    Change-Id: Ide93f4f5e7daa18522d4b65c00642ea812fbb847

--- src/sensor_fusion/vector.h
+++ src/sensor_fusion/vector.h
@@ -22,53 +22,53 @@
 
 #include "matrix.h"
 
-template <typename TYPE>
+template <typename TYPE,size_t SIZE>
 class vect {
 public:
-	int m_size;
-	TYPE *m_vec;
-
+	
+	TYPE m_vec[SIZE];
 	vect(void);
-	vect(const int size);
-	vect(const int size, TYPE *vec_data);
-	vect(const vect<TYPE>& v);
+	
+	vect(TYPE vec_data[SIZE]);
+	vect(const vect<TYPE,SIZE>& v);
 	~vect();
 
-	vect<TYPE> operator =(const vect<TYPE>& v);
+	vect<TYPE,SIZE> operator =(const vect<TYPE,SIZE>& v);
 
-	template<typename T> friend ostream& operator << (ostream& dout,
-			vect<T>& v);
-	template<typename T> friend vect<T> operator +(const vect<T> v1,
-			const vect<T> v2);
-	template<typename T> friend vect<T> operator +(const vect<T> v,
+	template<typename T,size_t S> friend ostream& operator << (ostream& dout,
+			vect<T,S>& v);
+	template<typename T,size_t S> friend vect<T,S> operator +(const vect<T,S> v1,
+			const vect<T,S> v2);
+	template<typename T,size_t S> friend vect<T,S> operator +(const vect<T,S> v,
 			const T val);
-	template<typename T> friend vect<T> operator -(const vect<T> v1,
-			const vect<T> v2);
-	template<typename T> friend vect<T> operator -(const vect<T> v,
+	template<typename T,size_t S> friend vect<T,S> operator -(const vect<T,S> v1,
+			const vect<T,S> v2);
+	template<typename T,size_t S> friend vect<T,S> operator -(const vect<T,S> v,
 			const T val);
-	template<typename T> friend matrix<T> operator *(const matrix<T> v1,
-			const vect<T> v2);
-	template<typename T> friend vect<T> operator *(const vect<T> v,
-			const matrix<T> m);
-	template<typename T> friend vect<T> operator *(const vect<T> v,
+	template<typename T,size_t S,size_t R,size_t C> friend matrix<T, R, S> operator *(const matrix<T,R,C> v1,
+			const vect<T,S> v2);
+	template<typename T,size_t S,size_t R,size_t C> friend vect<T,S> operator *(const vect<T,S> v,
+			const matrix<T, R, C> m);
+	template<typename T,size_t S> friend vect<T,S> operator *(const vect<T,S> v,
 			const T val);
-	template<typename T> friend vect<T> operator /(const vect<T> v1,
+	template<typename T,size_t S> friend vect<T,S> operator /(const vect<T,S> v1,
 			const T val);
-	template<typename T> friend bool operator ==(const vect<T> v1,
-			const vect<T> v2);
-	template<typename T> friend bool operator !=(const vect<T> v1,
-			const vect<T> v2);
+	template<typename T,size_t S1, size_t S2> friend bool operator ==(const vect<T,S1> v1,
+			const vect<T,S2> v2);
+	template<typename T,size_t S1, size_t S2> friend bool operator !=(const vect<T,S1> v1,
+			const vect<T,S2> v2);
 
-	template<typename T> friend T mul(const vect<T> v, const matrix<T> m);
-	template<typename T> friend void insert_end(vect<T>& v, T val);
-	template<typename T> friend matrix<T> transpose(const vect<T> v);
-	template <typename T> friend vect<T> transpose(const matrix<T> m);
-	template<typename T> friend vect<T> cross(const vect<T> v1,
-			const vect<T> v2);
-	template <typename T> friend T var(const vect<T> v);
-	template <typename T> friend bool is_initialized(const vect<T> v);
+	template<typename T,size_t S,size_t R,size_t C> friend T mul(const vect<T,S> v, const matrix<T,R,C> m);
+	template<typename T,size_t S> friend void insert_end(vect<T,S>& v, T val);
+	template<typename T,size_t S> friend matrix<T,S,1> transpose(const vect<T,S> v);
+	template <typename T,size_t R> friend vect<T,R> transpose(const matrix<T, R, 1> m);
+	template<typename T,size_t S> friend vect<T,S> cross(const vect<T,S> v1,
+			const vect<T,S> v2);
+	template <typename T,size_t S> friend T var(const vect<T,S> v);
+	template <typename T,size_t S> friend bool is_initialized(const vect<T,S> v);
 };
 
 #include "vector.cpp"
 
 #endif /* _VECTOR_H_ */
+
