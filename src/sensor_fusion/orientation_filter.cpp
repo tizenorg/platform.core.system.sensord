77b78437745e78d10128cb5b6db9561d3dd79ff9
Ramasamy <ram.kannan@samsung.com>
2015-02-25 10:18:07 +0530

    Replacing mul() with operator *() after rebase
    mul() method has been replaced with *() in one of the other
    commits. Rebasing according to the change.
    
    Change-Id: I1f332dbe3413f2b08e1107783c59cdca48f54c71

--- src/sensor_fusion/orientation_filter.cpp
+++ src/sensor_fusion/orientation_filter.cpp
@@ -304,7 +304,7 @@ inline void orientation_filter<TYPE>::time_update_gaming_rv()
 	m_measure_mat.m_mat[2][2] = 1;
 
 	if (is_initialized(m_state_old))
-		m_state_new = transpose(mul(m_tran_mat, transpose(m_state_old)));
+		m_state_new = transpose(m_tran_mat * transpose(m_state_old));
 
 	m_pred_cov = (m_tran_mat * m_pred_cov * tran(m_tran_mat)) + m_driv_cov;
 
