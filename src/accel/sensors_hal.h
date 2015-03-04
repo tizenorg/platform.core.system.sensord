21a49eb04dd0b1edf3bebd8de3972c2f3a69a871
Kibak Yoon <kibak.yoon@samsung.com>
2015-03-04 22:17:01 +0900

    sensord: temporary sensors HAL interface
    Change-Id: Ieecad4ec92634489fe477c98669f4e6e9a4e6b3d
    Signed-off-by: Kibak Yoon <kibak.yoon@samsung.com>

---
+++ src/accel/sensors_hal.h
@@ -0,0 +1,111 @@
+/*
+ * sensord
+ *
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef _SENSORS_HAL_INTERFACE_H_
+#define _SENSORS_HAL_INTERFACE_H_
+
+#define API __attribute__((visibility("default")))
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define SENSOR_DATA_VALUE_SIZE 16
+
+/**
+ * These APIs are used to control the sensors.
+*/
+
+typedef enum {
+	ALL_SENSOR = -1,
+	UNKNOWN_SENSOR = 0,
+	ACCELEROMETER_SENSOR,
+	GEOMAGNETIC_SENSOR,
+	LIGHT_SENSOR,
+	PROXIMITY_SENSOR,
+	THERMOMETER_SENSOR,
+	GYROSCOPE_SENSOR,
+	PRESSURE_SENSOR,
+	MOTION_SENSOR,
+	FUSION_SENSOR,
+	PEDOMETER_SENSOR,
+	CONTEXT_SENSOR,
+	FLAT_SENSOR,
+	BIO_SENSOR,
+	BIO_HRM_SENSOR,
+	AUTO_ROTATION_SENSOR,
+	GRAVITY_SENSOR,
+	LINEAR_ACCEL_SENSOR,
+	ROTATION_VECTOR_SENSOR,
+	GEOMAGNETIC_RV_SENSOR,
+	GAMING_RV_SENSOR,
+	ORIENTATION_SENSOR,
+	PIR_SENSOR,
+	PIR_LONG_SENSOR,
+	TEMPERATURE_SENSOR,
+	HUMIDITY_SENSOR,
+	ULTRAVIOLET_SENSOR,
+	DUST_SENSOR,
+	BIO_LED_GREEN_SENSOR,
+	BIO_LED_IR_SENSOR,
+	BIO_LED_RED_SENSOR,
+	RV_RAW_SENSOR,
+	UNCAL_GYROSCOPE_SENSOR,
+	UNCAL_GEOMAGNETIC_SENSOR
+} sensors_type_t;
+
+typedef struct sensors_data_t {
+	int accuracy;
+	unsigned long long timestamp;
+	int value_count;
+	float values[SENSOR_DATA_VALUE_SIZE];
+	float reserved[8];
+} sensors_data_t;
+
+struct sensors_module_t {
+	int (*initialize)(struct sensors_device_t *dev);
+
+	int (*get_info_list)(struct sensors_device_t *dev, int handle,
+			sensor_info_t **infos);
+
+	/* sensor must be given a handle(id)*/
+	int (*set_handle)(struct sensors_device_t *dev, int type, int *handle);
+
+	int (*get_type)(struct sensors_device_t *dev, int **types, int *count);
+
+	int (*enable)(struct sensors_device_t *dev, int handle, int enabled);
+
+	int (*set_interval)(struct sensors_device_t *dev, int handle,
+			unsigned long long interval_ns);
+
+	int (*get_fd)(struct sensors_device_t *dev, int handle, int *fd, int count);
+
+	int (*get_sensor_data)(struct sensors_device_t *dev, int handle, sensors_data_t *data);
+
+	int (*batch)(struct sensors_device_t *dev, int handle, int flags, unsigned long long interval_ns, unsigned long long max_report_latency_ns);
+
+	int (*flush)(struct sensors_device_t *dev, int handle);
+
+	int (*reserved_fp[8])(void);
+};
+
+API int create(const struct sensors_module_t** dev, int *count);
+
+#endif /* _SENSORS_HAL_H_ */
