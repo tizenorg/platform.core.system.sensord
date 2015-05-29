3fe701273ea18d32ef40175726e152146223d038
Ankur <ankur29.garg@samsung.com>
2015-05-29 14:33:13 +0530

    Adding tilt sensor event definitions [sensor_tilt.h] to avoid build break
    the sensor_tilt.h file containing the tilt sensor event definitions was missing.
    This was causing the build to break.
    Added the tilt sensor event definitions with tilt_raw_data_event.
    
    signed-off-by: Ankur Garg <ankur29.garg@samsung.com>
    Change-Id: I2d7768d70d05c7e4c1be08910276b958e76ef761

---
+++ src/libsensord/sensor_tilt.h
@@ -0,0 +1,52 @@
+/*
+ * libsensord
+ *
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef __SENSOR_TILT_H__
+#define __SENSOR_TILT_H__
+
+//! Pre-defined events for the tilt sensor
+//! Sensor Plugin developer can add more event to their own headers
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/**
+ * @defgroup SENSOR_TILT tilt Sensor
+ * @ingroup SENSOR_FRAMEWORK
+ *
+ * These APIs are used to control the tilt sensor.
+ * @{
+ */
+
+enum tilt_event_type {
+	TILT_RAW_DATA_EVENT 	= (TILT_SENSOR << 16) | 0x0001,
+};
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+//! End of a file
