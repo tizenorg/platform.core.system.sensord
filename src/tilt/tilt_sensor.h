b5897a28d782361d965c8bbe5062425db74a6214
Adarsh Shree Ram <adarsh.ram@samsung.com>
2015-08-13 17:30:17 +0530

    Code Cleanup for Tilt sensor files
    Change-Id: I0db7e52aa61812801342aa6d80a24fe2b78508f4

--- src/tilt/tilt_sensor.h
+++ src/tilt/tilt_sensor.h
@@ -48,11 +48,6 @@ private:
 
 	cmutex m_value_mutex;
 
-	orientation_filter<float> m_orientation_filter;
-	orientation_filter<float> m_orientation_filter_poll;
-
-	unsigned int m_enable_tilt;
-
 	unsigned long long m_time;
 	unsigned int m_interval;
 
