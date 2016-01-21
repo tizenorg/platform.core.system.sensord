3a1c4a357cf62fa345251dded841c940542198b4
kibak.yoon <kibak.yoon@samsung.com>
2016-01-21 16:18:10 +0900

    sensord: add sensor_event_poller for polling the event.
    * sensor_event_poller detects sensor_event from poll_fd.
    
    Change-Id: I29aecf10fdf504e20127bc958bb0503c1ea48b39
    Signed-off-by: kibak.yoon <kibak.yoon@samsung.com>

---
+++ src/server/sensor_event_poller.cpp
@@ -0,0 +1,143 @@
+/*
+ * libsensord-share
+ *
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <vector>
+#include <sensor_base.h>
+#include <physical_sensor.h>
+#include <sensor_event_poller.h>
+#include <sensor_plugin_loader.h>
+
+#define EPOLL_MAX_FD 32
+
+sensor_event_poller::sensor_event_poller()
+{
+	init_sensor_map();
+	init_fd();
+}
+
+sensor_event_poller::~sensor_event_poller()
+{
+}
+
+void sensor_event_poller::init_sensor_map()
+{
+	int fd;
+	physical_sensor *sensor;
+
+	std::vector<sensor_base *> sensors;
+	sensors = sensor_plugin_loader::get_instance().get_sensors(ALL_SENSOR);
+
+	auto it_sensor = sensors.begin();
+
+	while (it_sensor != sensors.end()) {
+		if ((*it_sensor)->is_virtual()) {
+			++it_sensor;
+			continue;
+		}
+
+		sensor = dynamic_cast<physical_sensor *>(*it_sensor);
+		fd = sensor->get_poll_fd();
+
+		m_fd_sensors.insert(std::make_pair(fd, sensor));
+		++it_sensor;
+	}
+}
+
+void sensor_event_poller::init_fd()
+{
+	fd_sensor_plugins::iterator it;
+	for (it = m_fd_sensors.begin(); it != m_fd_sensors.end(); it = m_fd_sensors.upper_bound(it->first)) {
+		if (!add_poll_fd(it->first))
+			continue;
+	}
+}
+
+bool sensor_event_poller::add_poll_fd(int fd)
+{
+	return m_poller.add_fd(fd);
+}
+
+bool sensor_event_poller::poll()
+{
+	while (true) {
+		int fd;
+		struct epoll_event poll_event;
+
+		if (!m_poller.poll(poll_event))
+			continue;
+
+		fd = poll_event.data.fd;
+
+		if (!is_data_ready(fd))
+			continue;
+
+		process_event(fd);
+	}
+
+	return true;
+}
+
+bool sensor_event_poller::is_data_ready(int fd)
+{
+	fd_sensor_plugins::iterator it;
+	physical_sensor *sensor;
+
+	it = m_fd_sensors.find(fd);
+	sensor = dynamic_cast<physical_sensor *>(it->second);
+
+	if (!sensor) {
+		ERR("Failed to get sensor plugin");
+		return false;
+	}
+
+	if (!sensor->is_data_ready())
+		return false;
+
+	return true;
+}
+
+bool sensor_event_poller::process_event(int fd)
+{
+	physical_sensor *sensor;
+	std::pair<fd_sensor_plugins::iterator, fd_sensor_plugins::iterator> ret;
+
+	ret = m_fd_sensors.equal_range(fd);
+
+	for (auto it_sensor = ret.first; it_sensor != ret.second; ++it_sensor) {
+		/*
+		sensor_event_t event;
+		sensor = it_sensor->second;
+
+		event.sensor_id = sensor->get_id();
+		event.event_type = sensor->get_event_type();
+		sensor->get_sensor_data(event.data);
+		*/
+		sensor_event_t *event;
+		int event_length;
+		sensor = it_sensor->second;
+
+		event_length = sensor->get_sensor_event(&event);
+		event->sensor_id = sensor->get_id();
+		event->event_type = sensor->get_event_type();
+
+		sensor->push(event, event_length);
+	}
+
+	return true;
+}
