2acc6df3d079bee00518839fe543e6fa1df51eaf
kibak.yoon <kibak.yoon@samsung.com>
2016-02-13 17:43:32 +0900

    sensord: add the sensord plugin logic for flexibility
    - if sensord has specific-type sensor plugin, then sensor_loader
      creates specific-type sensor object and set the handle info to it.
    - but sensord doesn't have specific-type sensor plugin based on
      physical_sensor, sensor_loader creates physical_sensor object
      basically and use it.
    
    Change-Id: Iceb5047d6db660c36a63d9deda1c45518eb24570
    Signed-off-by: kibak.yoon <kibak.yoon@samsung.com>

--- src/server/sensor_loader.h
+++ src/server/sensor_loader.h
@@ -38,15 +38,19 @@
 class sensor_base;
 
 typedef std::multimap<sensor_type_t, std::shared_ptr<sensor_base>> sensor_map_t;
+typedef std::map<const sensor_handle_t *, sensor_device *> sensor_device_map_t;
 
 class sensor_loader {
 private:
 	sensor_loader();
 
-	bool load_devices(const std::string &path, std::vector<sensor_device_t> &devices, void* &handle);
+	bool load_hal(const std::string &path, std::vector<sensor_device_t> &devices, void* &handle);
 
-	physical_sensor* create_sensor(sensor_handle_t handle, sensor_device *device);
-	bool load_physical_sensors(std::vector<sensor_device_t> &devices);
+	template <typename _sensor> sensor_base* create_sensor(void);
+
+	template <typename _sensor> void load_physical_sensor(sensor_type_t type = UNKNOWN_SENSOR);
+	bool load_devices(std::vector<sensor_device_t> &devices);
+	void load_physical_sensors(void);
 
 	template <typename _sensor> void load_virtual_sensor(const char *name);
 	void load_virtual_sensors(void);
@@ -55,6 +59,7 @@ private:
 	bool get_paths_from_dir(const std::string &dir_path, std::vector<std::string> &plugin_paths);
 
 	sensor_map_t m_sensors;
+	sensor_device_map_t m_devices;
 public:
 	static sensor_loader& get_instance();
 	bool load_sensors(void);
