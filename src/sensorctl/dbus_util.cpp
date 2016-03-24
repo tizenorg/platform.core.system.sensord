87a0c7392b322f262d7cc88997cf9e11a3d95c6e
kibak.yoon <kibak.yoon@samsung.com>
2016-03-24 15:22:23 +0900

    sensorctl: test for public
    Change-Id: I7edca88b4bd8c09089db547a4a380b4554af8f94
    Signed-off-by: kibak.yoon <kibak.yoon@samsung.com>

---
+++ src/sensorctl/dbus_util.cpp
@@ -0,0 +1,107 @@
+/*
+ * sensorctl
+ *
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <stdlib.h>
+#include <glib.h>
+#include <gio/gio.h>
+#include <sensorctl_log.h>
+
+static GDBusConnection *connection;
+
+bool dbus_init(void)
+{
+	GError *error = NULL;
+	gchar *gaddr;
+
+	if (connection)
+		return true;
+
+#ifndef GLIB_VERSION_2_36
+	g_type_init();
+#endif
+
+	gaddr = g_dbus_address_get_for_bus_sync(G_BUS_TYPE_SYSTEM, NULL, &error);
+
+	if (!gaddr) {
+		PRINT("ERROR: Failed to get dbus address : %s", error->message);
+		g_error_free(error);
+		error = NULL;
+		return false;
+	}
+
+	connection = g_dbus_connection_new_for_address_sync(gaddr,
+			(GDBusConnectionFlags)(G_DBUS_CONNECTION_FLAGS_AUTHENTICATION_CLIENT
+			| G_DBUS_CONNECTION_FLAGS_MESSAGE_BUS_CONNECTION),
+			NULL, NULL, &error);
+	g_free(gaddr);
+
+	if (!connection) {
+		PRINT("ERROR: Failed to get dbus connection : %s", error->message);
+		g_error_free(error);
+		error = NULL;
+		return false;
+	}
+
+	PRINT("G-DBUS connected[%s]\n",
+			g_dbus_connection_get_unique_name(connection));
+	return true;
+}
+
+bool dbus_fini(void)
+{
+	if (!connection)
+		return true;
+
+	g_dbus_connection_close_sync(connection, NULL, NULL);
+	g_object_unref(connection);
+	connection = NULL;
+
+	return true;
+}
+
+bool dbus_emit_signal(gchar *dest_bus_name, gchar *object_path,
+		gchar *interface_name, gchar *signal_name,
+		GVariant *variant, GError **error)
+{
+	g_dbus_connection_emit_signal(connection,
+				dest_bus_name,
+				object_path,
+				interface_name,
+				signal_name,
+				variant,
+				error);
+	return true;
+}
+
+GVariant *make_variant_int(int count, char *options[])
+{
+	switch (count) {
+	case 1:
+		return g_variant_new("(i)", atoi(options[0]));
+	case 2:
+		return g_variant_new("(ii)", atoi(options[0]), atoi(options[1]));
+	case 3:
+		return g_variant_new("(iii)", atoi(options[0]), atoi(options[1]), atoi(options[2]));
+	default:
+		break;
+	}
+
+	return NULL;
+}
+
---
+++ src/sensorctl/injector_wristup_conf.cpp
@@ -0,0 +1,59 @@
+/*
+ * sensorctl
+ *
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <stdio.h>
+#include <glib.h>
+#include <gio/gio.h>
+#include <sensorctl_log.h>
+#include "dbus_util.h"
+#include "injector_manager.h"
+#include "injector_wristup_conf.h"
+
+#define WRISTUP_CONF_SIGNAL			"conf"
+
+bool injector_wristup_conf::inject(int option_count, char *options[])
+{
+	GVariant *variant;
+
+	if (option_count == 0) {
+		_E("ERROR: invalid argument\n");
+		return false;
+	}
+
+	variant = make_variant_int(option_count, options);
+
+	if (variant == NULL)
+		return false;
+
+	dbus_emit_signal(NULL,
+			(gchar *)SENSORD_OBJ_PATH,
+			(gchar *)SENSORD_INTERFACE_NAME,
+			(gchar *)WRISTUP_CONF_SIGNAL,
+			variant,
+			NULL);
+
+	PRINT("set options to wristup: \n");
+	for(int i=0; i<option_count; ++i)
+		PRINT("option %d: %s\n", i, options[i]);
+
+	return true;
+}
+
+REGISTER_INJECTOR(MOTION_SENSOR, "conf", injector_wristup_conf)
+
