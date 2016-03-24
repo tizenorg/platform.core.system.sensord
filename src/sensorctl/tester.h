aeb32abe5610b2cc197114afbee8b4f293f473a7
kibak.yoon <kibak.yoon@samsung.com>
2016-03-24 15:22:23 +0900

    sensorctl: test for public
    Change-Id: I7edca88b4bd8c09089db547a4a380b4554af8f94
    Signed-off-by: kibak.yoon <kibak.yoon@samsung.com>

---
+++ src/sensorctl/tester.h
@@ -0,0 +1,31 @@
+/*
+ * sensorctl
+ *
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#pragma once // _TESTER_H_
+
+#include <sensor_internal.h>
+
+class tester_interface {
+public:
+	tester_interface() {}
+	virtual ~tester_interface() {}
+
+	virtual bool init(void) { return true; }
+	virtual bool test(sensor_type_t type, int option_count, char *options[]) = 0;
+};
