78a3fcaf3bb481235f4f4f1d8da91124fc305158
kibak.yoon <kibak.yoon@samsung.com>
2016-03-24 15:22:23 +0900

    xysensorctl: test for public
    Change-Id: I7edca88b4bd8c09089db547a4a380b4554af8f94
    Signed-off-by: kibak.yoon <kibak.yoon@samsung.com>

---
+++ src/sensorctl/injector_wristup_conf.cpp
@@ -0,0 +1,69 @@
+/*
+ * sensorctl
+ *
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <stdio.h>
+#include <glib.h>
+#include <gio/gio.h>
+#include "log.h"
+#include "dbus_util.h"
+#include "injector.h"
+
+#define WRISTUP_CONF_SIGNAL			"conf"
+
+namespace sensorctl {
+
+class injector_wristup_conf : public injector {
+public:
+	injector_wristup_conf(sensor_type_t sensor_type, const char *event_name);
+	virtual ~injector_wristup_conf() {}
+
+	bool inject(int argc, char *argv[]);
+};
+
+injector_wristup_conf::injector_wristup_conf(sensor_type_t sensor_type, const char *event_name)
+: injector(sensor_type, event_name)
+{
+}
+
+bool injector_wristup_conf::inject(int argc, char *argv[])
+{
+	GVariant *variant;
+
+	RETVM_IF(argc <= INJECTOR_ARGC, false, "Invalid argument\n");
+
+	variant = sensorctl::make_variant_int(argc - INJECTOR_ARGC, &argv[INJECTOR_ARGC]);
+	RETVM_IF(!variant, false, "Cannot make variant\n");
+
+	sensorctl::dbus_emit_signal(NULL,
+			(gchar *)SENSORD_OBJ_PATH,
+			(gchar *)SENSORD_INTERFACE_NAME,
+			(gchar *)WRISTUP_CONF_SIGNAL,
+			variant,
+			NULL);
+
+	_I("Set up options to wristup:");
+	for (int i = 0; i < argc - INJECTOR_ARGC; ++i)
+		_I("option %d: %s\n", i, argv[i]);
+
+	return true;
+}
+
+REGISTER_INJECTOR(GESTURE_WRIST_UP_SENSOR, WRISTUP_CONF_SIGNAL, injector_wristup_conf)
+
+} /* namespace sensorctl */
---
+++ src/sensorctl/tester_auto.cpp
@@ -0,0 +1,182 @@
+/*
+ * sensorctl
+ *
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <sensor_internal.h>
+
+#include "log.h"
+#include "tester.h"
+
+namespace sensorctl {
+
+class tester_auto : public tester {
+public:
+	bool test(sensor_type_t type, int option_count, char *options[]);
+};
+
+bool tester_auto::test(sensor_type_t type, int option_count, char *options[])
+{
+	tester_bench::run_all_testcase();
+	return true;
+}
+
+REGISTER_TESTER(ALL_SENSOR, "auto", tester_auto)
+
+} /* namespace sensorctl */
+
+static bool is_supported(sensor_type_t type)
+{
+	sensor_t sensor;
+
+	sensord_get_default_sensor(type, &sensor);
+	if (!sensor)
+		return false;
+
+	return true;
+}
+
+static void basic_cb(sensor_t sensor, unsigned int event_type, sensor_data_t *data, void *user_data)
+{
+	EXPECT_GT(data->timestamp, 0);
+	sensorctl::mainloop::stop();
+}
+
+static bool scenario_basic_p(sensor_t sensor)
+{
+	int handle;
+	int error;
+	bool result;
+
+	/*
+	error = sensord_get_default_sensor(type, &sensor);
+	ASSERT_IF_TEXT((error == 0), "manual.sensord_get_default_sensor");
+	*/
+
+	handle = sensord_connect(sensor);
+	ASSERT_IF_TEXT((handle >= 0), "manual.sensord_connect");
+
+	result = sensord_register_event(handle, SENSOR_EVENT(type), interval, latency, cb, user_data);
+	ASSERT_IF_TEXT((result == true), "manual.sensord_register_event");
+
+	result = sensord_start(handle, SENSOR_OPTION_ALWAYS_ON);
+	ASSERT_IF_TEXT((result == true), "manual.sensord_start");
+
+	sensor_data_t data;
+	result = sensord_get_data(handle, SENSOR_EVENT(type), &data);
+	EXPECT_IF_TEXT((result == true), "manual.sensord_get_data");
+
+	result = sensord_flush(handle);
+	EXPECT_IF_TEXT((result == true), "manual.sensord_flush");
+
+	sensorctl::mainloop::run();
+
+	result = sensord_unregister_event(handle, SENSOR_EVENT(type));
+	ASSERT_IF_TEXT((result == true), "manual.sensord_unregister_event");
+
+	result = sensord_stop(handle);
+	ASSERT_IF_TEXT((result == true), "manual.sensord_stop");
+
+	result = sensord_disconnect(handle);
+	ASSERT_IF_TEXT((result == true), "manual.sensord_disconnect");
+
+	return (m_fail == 0) ? true : false;
+}
+
+TESTCASE(all_sensor_scenario_basic_p)
+{
+	int error;
+	bool result;
+	sensor_t *sensors;
+	int count;
+
+	error = sensord_get_sensors(ALL_SENSOR, &sensors, &count);
+	ASSERT_IF(error == 0);
+
+	for (int i = 0; i < count; ++i) {
+		result = scenario_basic_p(sensors[i]);
+		EXPECT_IF(result == true);
+	}
+}
+
+#if 0
+TESTCASE(ALL, all_sensor_scenario_interval_10ms_p)
+{
+	int error;
+	bool result;
+	sensor_t *sensors;
+	int count;
+
+	error = sensord_get_sensors(ALL_SENSOR, &sensors, &count);
+	ASSERT_EQ(error, 0);
+
+	for (int i = 0; i < count; ++i) {
+		result = testcase_interval_10ms_p(sensors[i]);
+		EXPECT_EQ(result, true);
+	}
+}
+
+TESTCASE(ACCEL, accelerometer_value_p)
+{
+	scenario_basic_p(ACCELEROMETER_SENSOR);
+}
+
+TESTCASE(GYRO, gyroscope_value_p)
+{
+	scenario_basic_p(GYROSCOPE_SENSOR);
+}
+
+TESTCASE(GRAVITY, gravitye_value_p)
+{
+	scenario_basic_p(GRAVITY_SENSOR);
+}
+
+TESTCASE(LINEAR_ACCEL, linear_accel_value_p)
+{
+	value_p(LINEAR_ACCEL_SENSOR);
+}
+
+TESTCASE(PROXI, proximity_value_p)
+{
+	value_p(PROXIMITY_SENSOR);
+}
+
+TESTCASE(PRESSURE, pressure_value_p)
+{
+	value_p(PRESSURE_SENSOR);
+}
+
+TESTCASE(HRM, hrm_value_p)
+{
+	value_p(HRM_SENSOR);
+}
+
+TESTCASE(HRM_RAW, hrm_raw_value_p)
+{
+	value_p(HRM_RAW_SENSOR);
+}
+
+TESTCASE(HRM_LED_GREEN, hrm_led_green_value_p)
+{
+	value_p(HRM_LED_GREEN_SENSOR);
+}
+
+TESTCASE(WRIST_UP, wrist_up_value_p)
+{
+	value_p(GESTURE_WRIST_UP_SENSOR);
+}
+#endif
