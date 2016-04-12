98eba179ec6e560e1e33f6cd3b3f56f4852d7e61
kibak.yoon <kibak.yoon@samsung.com>
2016-04-13 00:30:35 +0900

    sensord: declare sensor types by using macro for converting type to string
    - added macros in enum_factory.h
    - DECLARE_ENUM
    - DECLARE_ENUM_UTIL
    - GENERATE_ENUM_UTIL
    
    - although it is hard to understand, it can reduce so many duplicated codes.
    
    Change-Id: Ice4436e5d232612e09e67aa9fdab4c2ac30a4ba4
    Signed-off-by: kibak.yoon <kibak.yoon@samsung.com>

---
+++ src/shared/enum_factory.h
@@ -0,0 +1,49 @@
+/*
+ * sensord
+ *
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef _ENUM_FACTORY_H_
+#define _ENUM_FACTORY_H_
+
+#define ENUM_SENSOR(name) name,
+#define ENUM_SENSOR_VALUE(name, assign) name = (assign),
+#define ENUM_SENSOR_DEPRECATED(name, assign) ENUM_SENSOR_VALUE(name, assign)
+
+#define ENUM_CASE(name) case (name): return #name;
+#define ENUM_CASE_VALUE(name, assign) ENUM_CASE(name)
+#define ENUM_CASE_IGNORE(name, assign)
+
+#define DECLARE_SENSOR_ENUM(ENUM_TYPE, ENUM_DEF) \
+	typedef enum ENUM_TYPE { \
+		ENUM_DEF(ENUM_SENSOR, ENUM_SENSOR_VALUE, ENUM_SENSOR_DEPRECATED) \
+	} ENUM_TYPE;
+
+#define DECLARE_SENSOR_ENUM_UTIL_NS(ENUM_TYPE) \
+	namespace util_##ENUM_TYPE { \
+		const char *get_string(ENUM_TYPE type); \
+	};
+
+#define DECLARE_SENSOR_ENUM_UTIL(ENUM_TYPE, ENUM_DEF) \
+	const char *util_##ENUM_TYPE::get_string(ENUM_TYPE type) { \
+		switch (type) { \
+		ENUM_DEF(ENUM_CASE, ENUM_CASE_VALUE, ENUM_CASE_IGNORE) \
+		} \
+		return "UNKNOWN"; \
+	}
+
+#endif /* _ENUM_FACTORY_H_ */
